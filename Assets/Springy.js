087609a6ed4fcd5ee59cae508ae3c961ea39022a
Michael Hill <channelcat@gmail.com>
2015-01-02 17:52:55 -0800

    Initial Commit

---
+++ Assets/Springy.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+private var springJoint : SpringJoint2D;
+
+function Start()
+{
+    springJoint = GetComponent(SpringJoint2D);
+    springJoint.connectedAnchor.x = this.gameObject.transform.position.x;
+    springJoint.connectedAnchor.y = this.gameObject.transform.position.y;
+}
+
+function Update () {
+
+}
