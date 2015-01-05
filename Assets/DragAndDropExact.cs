0e1e2b9d6f010b61dacf21b7b21be3aaffe5921f
Michael Hill <channelcat@gmail.com>
2015-01-04 16:43:52 -0800

    Added art assets

---
+++ Assets/DragAndDropExact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class DragAndDropExact : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	
+	void OnMouseDown() {
+		Debug.Log ("OMG");
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+}
