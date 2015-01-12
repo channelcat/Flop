41c6667e868eba489fde0601baa01a392aa3fa44
Michael Hill <channelcat@gmail.com>
2015-01-11 16:16:00 -0800

    Added new usable classes for lists and updated dragging script to use hit detection and velocity detection

--- Assets/DragAndDropExact.cs
+++ Assets/DragAndDropExact.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DragAndDropExact : MonoBehaviour {
 	private Vector3 screenPoint;
@@ -21,47 +22,66 @@ public class DragAndDropExact : MonoBehaviour {
 			}
 		}
 
-		Debug.Log(draggableRigidBody);
-		Debug.Log(dragCollider.isTrigger);
-
 		// Create a new collider in its own layer attached to the game object
 		GameObject colliderHolder = new GameObject("Drag Collider");
 		colliderHolder.transform.parent = dragCollider.gameObject.transform;
+        colliderHolder.transform.localPosition = new Vector3 (0,0,0);
 		colliderHolder.layer = LAYER;
 		CircleCollider2D newCollider = colliderHolder.AddComponent<CircleCollider2D>();
 		newCollider.radius = dragCollider.radius;
 		newCollider.center = dragCollider.center;
         newCollider.isTrigger = true;
 		Destroy (dragCollider);
-		dragCollider = newCollider;
+        dragCollider = newCollider;
 	}
 
 
 	public void Update(){
 		if (Input.GetMouseButtonDown (0)) {
 			//offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
-			Vector3 offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-			StartCoroutine ("DragObject", offset);
+            Vector3 mousePoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            RaycastHit2D hit = Physics2D.Raycast (mousePoint, Vector2.zero, Mathf.Infinity, LAYER_MASK);
+            Debug.Log (hit.collider);
+            if (hit.collider) {
+                Vector3 offset = gameObject.transform.position - mousePoint;
+                StartCoroutine ("DragObject", offset);
+            }
 		}
 	}
     IEnumerator DragObject(Vector3 offset) {
+        // Stop the ragdoll from interacting with the environment
         draggableRigidBody.isKinematic = true;
 
-        Vector3 startPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-        int ticks = 0;
+        int maxTicks = 5;
+        TailList<Tuple<float, Vector3>> ticks = new TailList<Tuple<float, Vector3>> (maxTicks);
+
 		while (Input.GetMouseButton(0)) {
+            // Move the ragdoll to the mouse location
 			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
 			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 			transform.position = curPosition;
-            ++ticks;
+
+            // Store his location
+            ticks.Add (new Tuple<float, Vector3>(Time.time, curPosition));
+
 			yield return null;
         }
 
-        draggableRigidBody.velocity = (transform.position - startPosition) / ticks * 300;
-        Debug.Log (ticks);
-        Debug.Log (transform.position - startPosition);
-        Debug.Log (draggableRigidBody.velocity);
+        // Determine velocity based off of last few ticks during the throw
+        Tuple<float, Vector3> lastTick = null;
+        Vector3 velocityVector = new Vector3();
+        int velocitySamples = 0;
+        foreach (Tuple<float, Vector3> tick in ticks) {
+            if (lastTick != null) {
+                ++velocitySamples;
+                velocityVector += (tick.Item2-lastTick.Item2) / (tick.Item1-lastTick.Item1);
+            }
+            lastTick = tick;
+        }
+        if (velocitySamples != 0)
+            velocityVector /= velocitySamples;
+
+        draggableRigidBody.velocity = velocityVector * 3;
         draggableRigidBody.isKinematic = false;
 	}
 }
